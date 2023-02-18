7619db11416456680df7a9f21abb6e3bb9204d6c
CCl₂F₂ <74624947+SokZKomucha@users.noreply.github.com>
2023-02-18 14:05:39 +0100

    Add files via upload

---
+++ 17022023-zadanie/scripts/sites/pseudoclassesListeners.js
@@ -0,0 +1,30 @@
+const menuEl1 = document.getElementById("listElement1");
+const menuEl2 = document.getElementById("listElement2");
+const menuEl3 = document.getElementById("listElement3");
+const menuEl4 = document.getElementById("listElement4");
+const back = document.getElementById("backButton");
+const gh = document.getElementById("github");
+
+menuEl1.addEventListener("click", function() {
+    window.location.href = "../index.html";
+});
+
+menuEl2.addEventListener("click", function() {
+    window.location.href = "classes.html";
+});
+
+menuEl3.addEventListener("click", function() {
+    window.location.href = "ids.html";
+});
+
+menuEl4.addEventListener("click", function() {
+    window.location.href = "elements.html";
+});
+
+back.addEventListener("click", function() {
+    window.location.href = "pseudoclasses.html#top";
+});
+
+gh.addEventListener("click", function() {
+    window.location.href = "https://github.com/SokZKomucha";
+})
--- 17022023-zadanie/scripts/sites/subsiteWindowWidth.js
+++ 17022023-zadanie/scripts/sites/subsiteWindowWidth.js
@@ -1,14 +1,14 @@
 const theme = document.getElementById("theme");
 
 if (window.outerWidth <= 1200)
-    theme.href = "styles/narrowScreens.css";
+    theme.href = "../styles/narrowScreens.css";
 else
-    theme.href = "styles/wideScreens.css";
+    theme.href = "../styles/wideScreens.css";
 
 window.addEventListener("resize", function () {
     console.log(window.outerWidth)
     if (window.outerWidth <= 1200)
-        theme.href = "styles/narrowScreens.css";
+        theme.href = "../styles/narrowScreens.css";
     else
-        theme.href = "styles/wideScreens.css";
+        theme.href = "../styles/wideScreens.css";
 });
